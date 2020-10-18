a918ff38dc80a638ba53b4a2673448e6b8f7377c
wheejuni <wheejuni@gmail.com>
2020-10-18 20:29:48 +0900

    container main app 작성

--- runtracker-frontend/src/Main.js
+++ runtracker-frontend/src/Main.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import App from './App'
 
 class MainApp extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = {};
+        this.setAuthenticationInfo = this.setAuthenticationInfo.bind(this);
+    }
+
+    setAuthenticationInfo(authenticationInfo) {
+        this.setState(
+            {
+                auth: authenticationInfo
+            }, () => {
+                console.log(`${this.state.auth}`)
+            }
+        );
+    }
+
+    render() {
+        console.log('this is main app.');
+        return(
+            <App />
+        )
     }
 }
+
+export default MainApp;
